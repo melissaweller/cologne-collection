03af479e7d371f1300e8339f83157a010b11288c
Melissa <112568989+melissaweller@users.noreply.github.com>
2026-03-12 11:32:56 -0400

    Add files via upload

---
+++ fragrances.js
@@ -0,0 +1,36 @@
+const fragrances = [
+
+{
+brand: "Chanel",
+name: "Bleu de Chanel",
+season: ["Spring","Summer"],
+time: ["Day"],
+top: "Lemon, Grapefruit",
+middle: "Ginger, Jasmine",
+base: "Sandalwood, Cedar",
+image: "https://fimgs.net/mdimg/perfume-thumbs/dark-375x500.25967.avif"
+},
+
+{
+brand: "Dior",
+name: "Sauvage",
+season: ["Spring","Summer","Fall","Winter"],
+time: ["Day","Night"],
+top: "Bergamot, Pepper",
+middle: "Lavender, Sichuan Pepper",
+base: "Ambroxan, Cedar",
+image: "https://fimgs.net/mdimg/perfume-thumbs/dark-375x500.31861.avif"
+},
+
+{
+brand: "YSL",
+name: "La Nuit de L'Homme",
+season: ["Fall","Winter"],
+time: ["Night"],
+top: "Cardamom",
+middle: "Lavender, Cedar",
+base: "Vetiver",
+image: "https://fimgs.net/mdimg/perfume-thumbs/dark-375x500.5521.avif"
+}
+
+];
